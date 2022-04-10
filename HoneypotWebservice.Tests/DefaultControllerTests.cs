25dfa85fd464c60b1140175f97912cbe9b820edd
Priyank Kothari <priyankkothari2008@gmail.com>
2022-04-10 23:24:17 +1200

    Added unit test project and wrote empty tests for DefaultController and StreamContent.

---
+++ HoneypotWebservice.Tests/DefaultControllerTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HoneypotWebservice.Tests
+{
+    [TestClass]
+    public class DefaultControllerTests
+    {
+        [TestMethod]
+        public void DefaultController_Returns_Ok()
+        {
+
+        }
+
+        [TestMethod]
+        public void DefaultController_Returns_BadRequest_IfCancellationRequested()
+        {
+
+        }
+
+        [TestMethod]
+        public void Default_Controller_Returns_InternalServerError()
+        {
+
+        }
+    }
+}
---
+++ HoneypotWebservice.Tests/StreamContentTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HoneypotWebservice.Tests
+{
+    [TestClass]
+    public class StreamContentTests
+    {
+        [TestMethod]
+        public void StreamContent_WritesStreamAscynchronously()
+        {
+
+        }
+
+        [TestMethod]
+        public void StreamContent_DelaysWritingStream_ForConfiguredDelay()
+        {
+
+        }
+    }
+}
